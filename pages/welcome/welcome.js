eed611c3626c5b8876413cab3e42a46983b36214
lollipop7 <juanlilu@yeah.net>
2018-01-17 00:44:02 +0800

    添加posts-data.js文件

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,1 +1,23 @@
-Page({})  
+Page({
+  onTap: function(){
+    // wx.navigateTo({
+    //   url: '../posts/posts',
+    // });
+    wx.redirectTo({
+      url: '../posts/posts',
+    })
+  },
+  /**
+ * 生命周期函数--监听页面隐藏
+ */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+})  
