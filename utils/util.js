f993eee00e8f31b98af80f913abac95ae48077a0
lollipop7 <juanlilu@yeah.net>
2018-01-26 18:30:40 +0800

    8-5 实现上滑加载更多数据

---
+++ utils/util.js
@@ -0,0 +1,34 @@
+function convertToStarsArray(stars) {
+  var num = stars.toString().substring(0, 1);
+  var array = [];
+  for (var i = 1; i <= 5; i++) {
+    if(i < num) {
+      array.push(1); //i小于评分，充入1
+    } else {
+      array.push(0);
+    }
+  }
+  return array;
+}
+
+function http(url, callback) {
+  wx.request({
+    url: url, 
+    method: 'GET',
+    header: {
+      "Content-Type": "application/json"
+    },
+    success: function (res) {
+      callback(res.data)
+    },
+    fail: function (error) {
+      console.log(error)
+    }
+  })
+}
+
+module.exports = {
+  convertToStarsArray,
+  http
+}
+
